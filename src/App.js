e3bdeb1796ac31e7b80a527503468a159f65d0b5
James Jeffeson <jrjefferson830@gmail.com>
2020-04-04 14:41:06 -0500

    update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,10 @@ import "bootstrap/dist/css/bootstrap.min.css/";
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">column number one</div>
-          <div className="col-6">
-            <span>
-              <i className="fas fa-home" />
-              </span>
-              </div>
-        </div>
-      </div>
+      <React.Fragment>
+        <h3>hello from app</h3>
+
+      </React.Fragment>
     );
   }
 }
