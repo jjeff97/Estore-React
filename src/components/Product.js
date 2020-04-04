bc909b11f3e53330762b27f93e1c9eef2467cf60
James Jeffeson <jrjefferson830@gmail.com>
2020-04-04 16:43:40 -0500

    route setup

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Poduct extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Product</h3>
+            </div>
+        )
+    }
+}
