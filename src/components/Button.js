1b469db5ff955ff3cc6553626fb03bed2326241d
James Jeffeson <jrjefferson830@gmail.com>
2020-07-03 21:30:35 -0500

    Button color customization

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,19 +6,22 @@ export const ButtonContainer = styled.button`
   background: transparent;
   border: 0.05rem solid var(--lightBlue);
   border-color: ${props =>
-    props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
-  color: ${prop => prop.cart ? "var(--mainYellow)" : "var(--lightBlue)"}; 
+  props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
+  color: ${prop => (prop.cart ? "var(--mainYellow)" : "var(--lightBlue)")};
   border-radius: 0.5rem;
   padding: 0.2rem 0.5rem;
   cursor: pointer;
   margin: 0.2rem 0.5rem 0.2rem 0;
-  transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-ease-in-out;
   &:hover {
-    background: ${prop => prop.cart ? "var(--mainYellow)" : "var(--lightBlue)"}; 
-     
+    background: ${prop => (prop.cart ? "var(--mainYellow)" : "var(--lightBlue)")};
     color: var(--mainBlue);
+  
+
   }
   &:focus {
     outline: none;
   }
+
+
 `;
