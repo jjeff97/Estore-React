a60446b0416db3059e87144abc4b1ef4c671ad27
James Jeffeson <jrjefferson830@gmail.com>
2020-07-03 22:22:48 -0500

    Functionality setup buttons

--- src/Context.js
+++ src/Context.js
@@ -22,9 +22,17 @@ class ProductProvider extends Component {
     this.setState(() => {
       return { products: tempProducts };
     });
-  }
-  handleDetail = () => {
-    console.log("hello from detial");
+  };
+getItem = (id) =>{
+  const product = this.state.products.find(item => item.id ===id);
+  return product;
+};
+
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(()=>{
+      return {detailProduct:product}
+    })
   };
   addToCart = (id) => {
     console.log(`hello from add to cart.id is ${id}`);
