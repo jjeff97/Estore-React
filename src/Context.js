5aab577e5c3f79d1802e24d900ed72dc74adc0b6
James Jeffeson <jrjefferson830@gmail.com>
2020-07-08 23:24:26 -0500

    Modal buttons setup

--- src/Context.js
+++ src/Context.js
@@ -10,7 +10,7 @@ class ProductProvider extends Component {
     products: [],
     detailProduct: detailProduct,
     cart: [],
-    modalOpen: true,
+    modalOpen: false,
     modalProduct: detailProduct,
   };
   componentDidMount() {
@@ -54,10 +54,10 @@ class ProductProvider extends Component {
       }
     );
   };
-  openModal = (id) => {
+  openModal = id => {
     const product = this.getItem(id);
     this.setState(() => {
-      return { modalProduct: product, modalProduct: true };
+      return { modalProduct: product, modalOpen: true };
     });
   };
   closeModal = () => {
