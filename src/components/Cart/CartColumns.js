c7d2737fb2ef5a7f3326d829d445bb91cc8bf85c
James Jeffeson <jrjefferson830@gmail.com>
2020-07-09 23:16:55 -0500

    Cart message setup

--- src/components/Cart/CartColumns.js
+++ src/components/Cart/CartColumns.js
@@ -11,16 +11,19 @@ export default function CartColumns() {
           <p className="text-uppercase">products</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">name of product</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">price</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">quantity</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">remove</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">total</p>
         </div>
       </div>
     </div>
