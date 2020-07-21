fc6b9d43f8806d3cca3157773bbe7accd399c06d
James Jeffeson <jrjefferson830@gmail.com>
2020-07-20 21:46:36 -0500

    paypal button ID encryption

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -30,8 +30,7 @@ export default class MyApp extends React.Component {
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox:
-        "AXCoiX9J7v-kERnKHSCU2Jsb96-LzLwUu37a_EqF3VvLi-Nb4BgLbGhCEiccWaCcX180Hh0sg8Uyv40D",
+      sandbox: process.env.REACT_APP_APP_ID,
       production: "YOUR-PRODUCTION-APP-ID",
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
